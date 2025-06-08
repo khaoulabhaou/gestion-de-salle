38ce56b043de0e874f320724a51f16e047ea995c
SEHUN <khaoulabhaou@gmail.com>
2025-06-08 06:14:56 +0100

    done

--- salle/resources/views/layouts/header.blade.php
+++ salle/resources/views/layouts/header.blade.php
@@ -3,30 +3,30 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-<nav class="main-nav position-relative">
-    <!-- Logo -->
-    <a href="{{ url('/') }}" class="logo">Training<em> Studio</em></a>
-
-    <!-- Menu trigger (only visible on mobile) -->
-    <a id="menuTrigger" class="menu-trigger d-md-none">
-        <span>Menu</span>
-    </a>
-
-    <!-- Nav items -->
-    <ul class="nav d-md-flex" id="navCollapse">
-        <li class="scroll-to-section"><a href="{{ url('') }}">Accueil</a></li>
-        <li class="scroll-to-section"><a href="{{ url('about') }}">À propos</a></li>
-        <li class="scroll-to-section"><a href="{{ url('classes') }}">Cours</a></li>
-        <li class="scroll-to-section"><a href="{{ url('schedules') }}">Horaires</a></li>
-        <li class="scroll-to-section"><a href="{{ url('contact') }}">Contact</a></li>
-        @guest
-            <li class="main-button"><a href="{{ route('register') }}">S'inscrire</a></li>
-        @endguest
-        @auth
-            <li class="main-button"><a href="{{ route('profile.edit') }}">Profil</a></li>
-        @endauth
-    </ul>
-</nav>
+                <nav class="main-nav position-relative">
+                    <!-- Logo -->
+                    <a href="{{ url('/') }}" class="logo">Training<em> Studio</em></a>
+                
+                    <!-- Menu trigger (only visible on mobile) -->
+                    <a id="menuTrigger" class="menu-trigger d-md-none">
+                        <span>Menu</span>
+                    </a>
+                
+                    <!-- Nav items -->
+                    <ul class="nav d-md-flex" id="navCollapse">
+                        <li class="scroll-to-section"><a href="{{ url('') }}">Accueil</a></li>
+                        <li class="scroll-to-section"><a href="{{ url('about') }}">À propos</a></li>
+                        <li class="scroll-to-section"><a href="{{ url('classes') }}">Cours</a></li>
+                        <li class="scroll-to-section"><a href="{{ url('schedules') }}">Horaires</a></li>
+                        <li class="scroll-to-section"><a href="{{ url('contact') }}">Contact</a></li>
+                        @guest
+                            <li class="main-button"><a href="{{ route('register') }}">S'inscrire</a></li>
+                        @endguest
+                        @auth
+                            <li class="main-button"><a href="{{ route('profile.edit') }}">Profil</a></li>
+                        @endauth
+                    </ul>
+                </nav>
             </div>
         </div>
     </div>
